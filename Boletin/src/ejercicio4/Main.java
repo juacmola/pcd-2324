99128b1b2f9b786f2e1f238b408112e5fd70de9f
juacmola <jorge.u.a@um.es>
2024-04-18 20:08:01 +0200

    La clase main del programa

--- Boletin/src/ejercicio4/Main.java
+++ Boletin/src/ejercicio4/Main.java
@@ -3,23 +3,26 @@ package ejercicio4;
 import messagepassing.MailBox;
 
 public class Main {
-
+//	private static int testigo;
 	public static void main(String[] args) {
-		MailBox buzonControladorComprador = new MailBox();
-		MailBox buzonR2 = new MailBox();
+		MailBox buzonPregunta = new MailBox();
+		MailBox cajaA = new MailBox();
+		MailBox cajaB = new MailBox();
+		MailBox buzonRespuesta = new MailBox();
+		MailBox imprimir = new MailBox();
 		
+		MailBox[] arrayBuzon = new MailBox[30];
+		for(int i = 0; i < 30; i++) arrayBuzon[i] = new MailBox(); 
 		
-		HiloControlador controlador = new HiloControlador(buzonControladorComprador);
+//		imprimir.send(testigo);
+		
+		HiloControlador controlador = new HiloControlador(buzonPregunta, cajaA, cajaB, buzonRespuesta, arrayBuzon, imprimir);
 		controlador.start();
 		
 		HiloComprador[] compradores = new HiloComprador[30];
 		
-		for(int i=0;i<30;i++) {
-			compradores[i]=new HiloComprador(i, buzonControladorComprador);
-		}
-		for(int i=0; i< 30; i++) {
-			compradores[i].start();
-		}
+		for(int i=0;i<30;i++) compradores[i]=new HiloComprador(i, buzonPregunta, cajaA, cajaB, buzonRespuesta, arrayBuzon, imprimir);
+		for(int i=0; i< 30; i++) compradores[i].start();
 	}
 
 }
